5420fe6a952c4b89cb3f9a47607dae5946d00f53
shawn00-tech <shawnjay001@gmail.com>
2022-05-10 12:22:37 +0545

    Update train.py

--- train.py
+++ train.py
@@ -17,12 +17,10 @@ n_classes = 60
 
 model = nn.Sequential(OrderedDict([
     ('frontend', conv_3d()),
-    ('features', TimeDistributed(DenseNet())),
-    ('backend', SEQ(input_size=5000, hidden_size1=2500)),
-    ('fc', nn.Sequential( nn.Dropout(p=0.5), nn.Linear(2500, n_classes) ))
+    ('mid', conv_2d()),
+    ('fc', nn.Sequential( nn.Dropout(p=0.4), nn.Linear(135168, 8448), nn.Linear(8448, 1024), nn.Linear(1024,256), nn.Linear(256,60) ))
 ]))
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 # specify loss function (categorical cross-entropy)
@@ -31,8 +29,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(
   [
         {"params": model.fc.parameters(), "lr": 1e-3},
-        {"params": model.backend.parameters(), "lr": 1e-5},
-        {"params": model.features.parameters(), "lr": 1e-4},
+        {"params": model.mid.parameters(), "lr": 1e-5},
         {"params": model.frontend.parameters(), "lr": 1e-4},
   ],
   momentum = 0.9
@@ -40,35 +37,42 @@ optimizer = optim.SGD(
 
 
 def train(model, device, train_loader, optimizer, criterion, epoch):
+    model.cuda()
     model.train()
     train_loss = 0
     correct = 0
+    num_samples = 0
     
-    train_steps = train_loader.get_sample_size()
+    for epoch in tqdm(range(epoch)):
+        for batch_idx, (data, target) in enumerate(train_loader):
+            
+            data = data.to(device)
+            target = torch.tensor(target).to(device)
+
     
-    gen = train_loader.generator()
     
-    for batch_idx in tqdm(range(train_steps)): 
-        data, target = next(gen)
-        data, target = data.to(device), torch.max(target.long().to(device), 1)[1]
-        optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step()
-        train_loss += loss.item()
-        pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
-        correct += pred.eq(target.view_as(pred)).sum().item()
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            
+            train_loss += loss.item()
+            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+            num_samples += pred.shape[0]
+            print(num_samples)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            print("correct till now:",correct)
         
-    train_loss /= train_loader.get_sample_size()
+    train_loss /= num_samples
     print('Epoch: {} , Training Accuracy: {}/{} ({:.0f}%) Training Loss: {:.6f}'.format(
-                epoch, correct, train_loader.get_sample_size(),
-                100. * correct / train_loader.get_sample_size(), train_loss))
+                epoch, correct, num_samples,
+                100. * correct / num_samples, train_loss))
 
 
-dataloader = load_dataloader(batch_size=8)
+dataloader = load_dataloader(batch_size=1)
 
-x,y = iter((dataloader)).next()
-print(y)
 
-#train(model,device,dataloader,optimizer,criterion,4)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+train(model,device,dataloader,optimizer,criterion,1)
