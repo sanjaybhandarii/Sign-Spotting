75853ad7ea19433eb87f719280d20d49570892b6
shawn00-tech <shawnjay001@gmail.com>
2022-05-13 17:17:56 +0545

    Delete practice.py

--- practice.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-
-x = torch.randn(1, 3,5, 64, 64)
-x = torch.tensor(torch.unbind(x, dim=4))
-
-
-y = nn.Conv2d(3, 64, kernel_size=5, padding=1)(x)
-print(y.shape)
