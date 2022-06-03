8fcbb4d2e48c97da0c90f9e00c7ad92b039056b3
shawn00-tech <shawnjay001@gmail.com>
2022-06-03 18:38:46 +0545

    practice

--- practice.py
+++ practice.py
@@ -1,64 +1,78 @@
 import pickle
+from re import S
 import numpy as np
 from collections import Counter
 
 with open('MSSL_TRAIN_SET_GT.pkl', 'rb') as f:
         data = pickle.load(f)
 
+
 classes= []
+start = []
+end = []
 
 
 # keys are files so iterate only limited files due to memory limitations.
-for key in list(data.keys()):
+for key in list(data.keys())[0:1]:
     filename = key
     # file functions
-
     for x in data[key]:
-        classes.append(x[0])
+        start.append(x[1])
+        end.append(x[2])
+
+print(start)
+print(end)
+
+for i in range(len(start)-1):
+    if end[i]- start[i+1]>550:
+            start = x[0]
+            end = x[1]
         # start_time = x[1]
         # end_time = x[2]
         #diff = x[2] - x[1]
         
 
 
-x = dict(Counter(classes))
-count = dict(sorted(x.items(), key=lambda item: item[1]))
+# x = dict(Counter(classes))
+# count = dict(sorted(x.items(), key=lambda item: item[1]))
 
-print("Count of classes:")
-print(x,"\n")
-time = dict.fromkeys(x.keys(),0)
+# print("Count of classes:")
+# print(x,"\n")
+# time = dict.fromkeys(x.keys(),0)
+# # print(time)
+
+
+# for key in list(data.keys()):
+#     filename = key
+#     # file functions
+
+#     for x in data[key]:
+#         #classes.append(x[0])
+#         diff = x[2] - x[1]
+#         # print(x[0])
+#         #time[x[0]].append(diff)
+#         if time[x[0]] < diff:
+#             time[x[0]] = diff
+#             if diff>= 1400:
+#                     print(filename,x[0],diff)
+# print("Longest Duration of each class:")
+# print(dict(sorted(time.items(), key=lambda item: item[1])))
+
 # print(time)
 
+# for key in list(data.keys()):
+#     filename = key
+#     # file functions
 
-for key in list(data.keys()):
-    filename = key
-    # file functions
-
-    for x in data[key]:
-        #classes.append(x[0])
-        diff = x[2] - x[1]
-        # print(x[0])
-        #time[x[0]].append(diff)
-        if time[x[0]] < diff:
-            time[x[0]] = diff
-print("Longest Duration of each class:")
-#print(dict(sorted(time.items(), key=lambda item: item[1])))
-
-print(time)
-
-for key in list(data.keys()):
-    filename = key
-    # file functions
-
-    for x in data[key]:
-        #classes.append(x[0])
-        diff = x[2] - x[1]
-        # print(x[0])
-        #time[x[0]].append(diff)
-        if time[x[0]] > diff:
-            time[x[0]] = diff
-print("Shortest Duration of each class:")
-print(time)
+#     for x in data[key]:
+#         #classes.append(x[0])
+#         diff = x[2] - x[1]
+#         # print(x[0])
+#         #time[x[0]].append(diff)
+#         if time[x[0]] > diff:
+#             time[x[0]] = diff
+# print("Shortest Duration of each class:")
+# print(time)
 
 
 
--- test.py
+++ test.py
@@ -1,24 +1,5 @@
-
 import torch
-from torch.nn.utils.rnn import pad_sequence
-a = torch.randn(5,3,7,8,8)
+src = torch.Tensor([[1, 2, 3], [4, 5, 6]])
 
 
-# c = torch.randn(2,3,7,8,8)
-
-# print("a:",a)
-
-# print("\n c:",c)
-# d = pad_sequence([a,c],batch_first=True)
-# a,c = [m for m in torch.unbind(d, dim=0)]
-# print("\n a:",a)
-
-# print("\n c:",c)
-
-
-def pad_constant(tensor, length, value):
-    return torch.cat([tensor, tensor.new_zeros(length - tensor.size(0), *tensor.size()[1:])], dim=0)
-
-f = pad_constant(a,41,0)
-
-print(f.shape)
+print(src.transpose(0, 1))
